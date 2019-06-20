d71dc6cd7832ae0121c19300d15acb679794c8d6
Justine Robert Igune <justine.igune@stud.kiu.ac.ug>
2019-06-20 14:54:47 +0300

    Combining different modules together in JavaScript

---
+++ src/data/dogData.js
@@ -0,0 +1,7 @@
+// Rewrite "./dog" (relative path)
+import Dog from "../class/dog";
+
+const dog1 = new Dog("John", 5, "Chihuahua");
+const dog2 = new Dog("Shadrachi", 3, "Poodle");
+
+export { dog1, dog2 };
